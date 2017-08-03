7282809e84504ae1069b080cd9a2aa2400f33c5a
Shougo Suzuki <s1220032@Shogo-no-Mac-mini.local>
2017-08-03 18:56:16 +0900

    modify primitive

--- src/Model2.js
+++ src/Model2.js
@@ -11,8 +11,8 @@ model.eval = function(x, y, z) {
     qy = py * Math.cos(-0.9) + pz * Math.sin(-0.9);
     qz = -py * Math.sin(-0.9) + pz * Math.cos(-0.9);
 
-    py = qy + 3.3;
-    pz = qz + 3.5;
+    py = qy - 3.3;
+    pz = qz - 3.5;
 
     var brim = Primitives.torusY(px, py, pz, center, 2.9, 0.5);
 
@@ -21,20 +21,20 @@ model.eval = function(x, y, z) {
     py = y * Math.cos(-0.9) + z * Math.sin(-0.9);
     pz = -y * Math.sin(-0.9) + z * Math.cos(-0.9);
 
-    py = py + 2.8;
-    pz = pz + 3.5;
+    py = py - 2.8;
+    pz = pz - 3.5;
 
     var cap1 = Primitives.ellipsoid(px, py, pz, center, 3.1, 4.0, 3.35);
-    cap1 = Operations.rSub(cap1, (py - 0.5));
+    cap1 = Operations.rSub(cap1, -(py - 0.5));
 
     center = [0.0, 2.2, -3.3];
-    var cap2 = Primitives.ellipsoid(x, 0, 0, center, 1.4, 3.2, 0.6);
+    var cap2 = Primitives.ellipsoid(x, y, z, center, 1.4, 3.2, 0.6);
     cap2 = Operations.rSub(cap2, (y - 5.5));
     var cap3 = Operations.rBUnion(cap1, cap2, 0.1, 0.1, 0.1);
 
     // tassel
     center = [0.0, 2.2, -3.9];
-    var tassel = Primitives.sphere(x, 0, 0, center, 0.8);
+    var tassel = Primitives.sphere(x, y, z, center, 0.8);
     var cap = Operations.rUnion(cap3, brim);
     cap = Operations.rUnion(cap, tassel);
 
@@ -42,8 +42,8 @@ model.eval = function(x, y, z) {
 py = y * Math.cos(-0.3) + z * Math.sin(-0.3);
 pz = -y * Math.sin(-0.3) + z * Math.cos(-0.3);
 
-py = py + 1.7;
-pz = pz + 0.27;
+py = py - 1.7;
+pz = pz - 0.27;
 
 center = [2.5, 5.6, 1.0];
 var ear1 = Primitives.ellipsoid(px, py, pz, center, 1.5, 1.5, 0.9);
@@ -61,10 +61,10 @@ var ear = Operations.rUnion(earLeft, earRight);
 
 // hear
 center = [0.0, 4.0, 0.5];
-var head1 = Primitives.ellipsoid(x, 0, 0, center, 2.7, 3.0, 2.8);
+var head1 = Primitives.ellipsoid(x, y, z, center, 2.7, 3.0, 2.8);
 
 center = [0.0, 5.0, 0.9];
-var head2 = Primitives.ellipsoid(x, 0, 0, center, 2.0, 2.0, 2.1);
+var head2 = Primitives.ellipsoid(x, y, z, center, 2.0, 2.0, 2.1);
 var head3 = 4.0 - y;
 var head4 = z - 2.5;
 var hear = Operations.rSub(head1, head2);
@@ -74,29 +74,29 @@ hear = Operations.rSub(hear, (2.3 - y));
 
 // face
 center = [0.0, 4.0, 0.5];
-var face1 = Primitives.ellipsoid(x, 0, 0, center, 2.3, 2.9, 2.4);
+var face1 = Primitives.ellipsoid(x, y, z, center, 2.3, 2.9, 2.4);
 var face = Operations.rSub(face1, hear2);
 face = Operations.rSub(face, (2.3 - y));
 
 // eyes
 center = [0.0, 4.0, 0.6];
-var eye1 = Primitives.ellipsoid(x, 0, 0, center, 2.3, 2.9, 2.4);
+var eye1 = Primitives.ellipsoid(x, y, z, center, 2.3, 2.9, 2.4);
 
 center = [0.5, 5.5, 0.0];
-var eye2 = Primitives.ellCylZ(x, 0, 0, center, 0.4, 0.7);
+var eye2 = Primitives.ellCylZ(x, y, z, center, 0.4, 0.7);
 var eye3 = Operations.rInter(eye1, eye2);
 
 center = [-0.5, 5.5, 0.0];
-var eye4 = Primitives.ellCylZ(x, 0, 0, center, 0.4, 0.7);
+var eye4 = Primitives.ellCylZ(x, y, z, center, 0.4, 0.7);
 var eye5 = Operations.rInter(eye1, eye4);
 
     // iris
     center = [0.5, 5.2, 2.65];
-    var eye6 = Primitives.sphere(x, 0, 0, center, 0.28);
+    var eye6 = Primitives.sphere(x, y, z, center, 0.28);
     var eye7 = Operations.rInter(eye3, -eye6);
 
     center = [-0.5, 5.2, 2.65];
-    var eye8 = Primitives.sphere(x, 0, 0, center, 0.28);
+    var eye8 = Primitives.sphere(x, y, z, center, 0.28);
     var eye9 = Operations.rInter(eye5, -eye8);
 
 // cheek and mouth
@@ -142,13 +142,13 @@ var nose1 = Primitives.ellipsoid(px, py, pz, center, 1.97, 1.5, 5.5);
 nose1 = Operations.rSub(nose1, (1.0 - pz));
 
 center = [0.0, 5.0, 3.9];
-var nose2 = Primitives.ellipsoid(x, 0, 0, center, 0.5, 0.7, 0.5);
+var nose2 = Primitives.ellipsoid(x, y, z, center, 0.5, 0.7, 0.5);
 
 var nose = Operations.rUnion(nose1, nose2);
 
 // neck
 center = [0.0, 1.9, 0.0];
-var neck = Primitives.torusY(x, 0, 0, center, 1.0, 0.8);
+var neck = Primitives.torusY(x, y, z, center, 1.0, 0.8);
 
 var head = Operations.rUnion(hear, face);
 head = Operations.rUnion(head, eye7);
@@ -159,7 +159,7 @@ head = Operations.rUnion(head, nose);
 
 // breast
 center = [0.0, 0.1, -1.7];
-var body1 = Primitives.ellipsoid(x, 0, 0, center, 2.0, 1.6, 3.4);
+var body1 = Primitives.ellipsoid(x, y, z, center, 2.0, 1.6, 3.4);
 body1 = Operations.rSub(body1, ((-1.7) - z));
 
 // arm
@@ -193,17 +193,17 @@ arm2 = Operations.rSub(arm2, (pz + 1.6));
 
     // cuffs
     center = [2.6, -3.0, -2.2];
-    var cuff1 = Primitives.torusY(x, 0, 0, center, 0.6, 0.6);
+    var cuff1 = Primitives.torusY(x, y, z, center, 0.6, 0.6);
     center = [-2.6, -3.0, -2.2];
-    var cuff2 = Primitives.torusY(x, 0, 0, center, 0.6, 0.6);
+    var cuff2 = Primitives.torusY(x, y, z, center, 0.6, 0.6);
 
     // hands
     center = [2.6, -4.3, -2.0];
-    var hand1 = Primitives.ellipsoid(x, 0, 0, center, 0.8, 1.7, 2.5);
+    var hand1 = Primitives.ellipsoid(x, y, z, center, 0.8, 1.7, 2.5);
     hand1 = Operations.rSub(hand1, ((-4.6) - y));
 
     center = [-2.6, -4.3, -2.0];
-    var hand2 = Primitives.ellipsoid(x, 0, 0, center, 0.8, 1.7, 2.5);
+    var hand2 = Primitives.ellipsoid(x, y, z, center, 0.8, 1.7, 2.5);
     hand2 = Operations.rSub(hand2, ((-4.6) - y));
 
     var armLeft = Operations.rUnion(arm1, cuff1);
@@ -215,18 +215,18 @@ arm2 = Operations.rSub(arm2, (pz + 1.6));
 // lower half of body
     //　belt
     center = [0.0, -4.2, -0.5];
-    var belt1 = Primitives.torusY(x, 0, 0, center, 1.9, 0.8);
+    var belt1 = Primitives.torusY(x, y, z, center, 1.9, 0.8);
     belt1 = Operations.rSub(belt1, ((-1.6) - z));
-    var belt2 = Primitives.cylY(x, 0, 0, center, 1.8);
+    var belt2 = Primitives.cylY(x, y, z, center, 1.8);
 
     var belt = Operations.rInter(belt1, belt2);
 
     // belly
     center = [0.0, -4.2, -2.0];
-    var belly1 = Primitives.ellipsoid(x, 0, 0, center, 4.0, 2.9, 5.0);
+    var belly1 = Primitives.ellipsoid(x, y, z, center, 4.0, 2.9, 5.0);
     belly1 = Operations.rSub(belly1, ((-4.6) - y));
     belly1 = Operations.rSub(belly1, ((-2.0) - z));
-    var belly2 = Primitives.ellipsoid(x, 0, 0, center, 4.0, 2.9, 1.0);
+    var belly2 = Primitives.ellipsoid(x, y, z, center, 4.0, 2.9, 1.0);
     belly2 = Operations.rSub(belly2, ((-4.6) - y));
 
     var belly = Operations.rUnion(belly1, belly2);
@@ -236,21 +236,21 @@ arm2 = Operations.rSub(arm2, (pz + 1.6));
 
 // leg
 center = [0.0, -4.0, 19.8];
-var leg1 = Primitives.torusY(x, 0, 0, center, 20.0, 1.5);
+var leg1 = Primitives.torusY(x, y, z, center, 20.0, 1.5);
 leg1 = Operations.rInter(leg1, (x + 4.5));
 leg1 = Operations.rInter(leg1, (4.5 - x));
 
 center = [4.5, -4.0, 0.0];
-var hem1 = Primitives.torusX(x, 0, 0, center, 1.6, 0.4);
+var hem1 = Primitives.torusX(x, y, z, center, 1.6, 0.4);
 center = [-4.5, -4.0, 0.0];
-var hem2 = Primitives.torusX(x, 0, 0, center, 1.6, 0.4);
+var hem2 = Primitives.torusX(x, y, z, center, 1.6, 0.4);
 
 center = [5.9, -3.0, 0.0];
-var boots1 = Primitives.ellipsoid(x, 0, 0, center, 1.5, 3.5, 1.9);
+var boots1 = Primitives.ellipsoid(x, y, z, center, 1.5, 3.5, 1.9);
 boots1 = Operations.rSub(boots1, (x - 5.9));
 boots1 = Operations.rSub(boots1, ((-6.0) - y));
 center = [-5.9, -3.0, 0.0];
-var boots2 = Primitives.ellipsoid(x, 0, 0, center, 1.5, 3.5, 1.9);
+var boots2 = Primitives.ellipsoid(x, y, z, center, 1.5, 3.5, 1.9);
 boots2 = Operations.rSub(boots2, ((-5.9) - x));
 boots2 = Operations.rSub(boots2, ((-6.0) - y));
 
@@ -269,9 +269,9 @@ return my_model;
 }
 
 model.boundingBox = function() {
-    var bbox = {x_min: -7, x_max: 7, 
-		y_min: -7, y_max: 7,
-		z_min: -7, z_max: 7}
+    var bbox = {x_min: -10, x_max: 10, 
+		y_min: -10, y_max: 10,
+		z_min: -10, z_max: 10}
 
     return bbox;
 }
--- src/Primitives.js
+++ src/Primitives.js
@@ -57,6 +57,13 @@ Primitives.cylY = function(x, y, z, center, radius) {
     return radius*radius - xt*xt - zt*zt;
 }
 
+Primitives.cylZ = function(x, y, z, center, radius) {
+    var xt = x - center[0];
+    var yt = y - center[1];
+
+    return radius*radius - xt*xt - yt*yt;
+}
+
 Primitives.coneX = function(x, y, z, center, radius) {
     var xt = x - center[0];
     var yt = y - center[1];
@@ -65,12 +72,44 @@ Primitives.coneX = function(x, y, z, center, radius) {
     return xt*xt - (yt*yt / radius*radius) - (zt*zt / radius*radius);
 }
 
+Primitives.coneY = function(x, y, z, center, radius) {
+    var xt = x - center[0];
+    var yt = y - center[1];
+    var zt = z - center[2];
+
+    return yt*yt - (xt*xt / radius*radius) - (zt*zt / radius*radius);
+}
+
+Primitives.coneZ = function(x, y, z, center, radius) {
+    var xt = x - center[0];
+    var yt = y - center[1];
+    var zt = z - center[2];
+
+    return zt*zt - (xt*xt / radius*radius) - (yt*yt / radius*radius);
+}
+
 Primitives.ellConeX = function(x, y, z, center, a, b) {
     var xt = x - center[0];
     var yt = y - center[1];
     var zt = z - center[2];
 
     return xt*xt - (yt*yt / a*a) - (zt*zt / b*b);
+}
+
+Primitives.ellConeY = function(x, y, z, center, a, b) {
+    var xt = x - center[0];
+    var yt = y - center[1];
+    var zt = z - center[2];
+
+    return yt*yt - (xt*xt / a*a) - (zt*zt / b*b);
+}
+
+Primitives.ellConeZ = function(x, y, z, center, a, b) {
+    var xt = x - center[0];
+    var yt = y - center[1];
+    var zt = z - center[2];
+
+    return zt*zt - (xt*xt / a*a) - (yt*yt / b*b);
 }
 
 Primitives.torusX = function(x, y, z, center, radius, radius0) {
@@ -98,4 +137,202 @@ Primitives.torusZ = function(x, y, z, center, radius, radius0) {
 
     return radius0*radius0 - xt*xt - yt*yt - zt*zt - radius*radius
             + 2*radius*Math.sqrt(xt*xt + yt*yt);
+}
+
+Primitives.superEllipsoid = function(x, y, z, center, a, b, c, s1, s2) {
+    var xt = x - center[0];
+    var yt = y - center[1];
+    var zt = z - center[2];
+
+    return 1 - Math.pow((Math.pow((xt/a), 2)/s2 + Math.pow((yt/b), 2)/s2), (s2/s1))
+            - Math.pow((zt/c), (2/s1));
+}
+
+Primitives.block = function(x, y, z, vertex, dx, dy, dz) {
+    var xt = (x - vertex[0]) * (vertex[0] + dx - x);
+    var yt = (y - vertex[1]) * (vertex[1] + dy - y);
+    var zt = (z - vertex[2]) * (vertex[2] + dz - z);
+
+    var f = (xt < yt) ? xt : yt;
+
+    return (f < zt) ? f : zt;
+}
+
+primitives.blobby = function(x, y, z, center, a, b, t) {
+    var xt = x - center[0];
+    var yt = y - center[1];
+    var zt = z - center[2];
+
+    var blobby = b * Math.exp(-a*(xt*xt + yt*yt + zt*zt));
+
+    return (blobby - t);
+}
+
+primitives.metaball = function(x, y, z, center, b, d, t) {
+    var xt = x - center[0];
+    var yt = y - center[1];
+    var zt = z - center[2];
+
+    var r = Math.sqrt(xt*xt + yt*yt + zt*zt);
+    var rd = r / d;
+
+    if (r <= d/3) {
+        var mb = b * (1 - 3*rd*rd);
+    } else var mb = (r < d) ? 1.5*b*(1 - rd)*(1 - rd) : mb;
+
+    return (mb - t);
+}
+
+primitives.soft = function(x, y, z, center, d, t) {
+    var xt = x - center[0];
+    var yt = y - center[1];
+    var zt = z - center[2];
+
+    var r2 = xt*xt + yt*yt + zt*zt;
+    var r4 = r2*r2;
+    var d2 = d*d;
+    var d4 = d2*d2;
+
+    var soft = (Math.sqrt(r2) <= d) ? soft + 1 - 22*r2/(9*d2) + 17*r4/(9*d4) 
+                - 4*r4*r2/(9*d4*d2) : soft;
+
+    return (soft - t);
+}
+
+primitives.convPoint = function(x, y, z, vx, vy, vz, s, t) {
+    var r2 = SQ(vx - x) + SQ(vy - y) + SQ(vz - z);
+    var f = 1/SQ(1 + SQ(s)*r2);
+
+    return f - t;
+}
+
+primitives.convLine = function(x, y, z, bx, by, bz, ex, ey, ez, s, t) {
+    var I = sqrt(SQ(ex - bx) + SQ(ey - by) + SQ(ez - bz));
+
+    if (I == 0.0) {
+        printf("ERROR:Tips of the segment take same coordinate!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    var ax = (ex - bx)/I;
+    var ay = (ey - by)/I;
+    var az = (ez - bz)/I;
+
+    var dx = x - bx;
+    var dy = x - by;
+    var dz = x - bz;
+
+    var xx = dx*ax + dy*ay + dz*az;
+    var p = Math.sqrt(1 + s*s*(dx*dx + dy*dy + dz*dz - xx*xx));
+    var q = Math.sqrt(1 + s*s*(dx*dx + dy*dy + dz*dz - 2*xx));
+
+    var f = xx/(2*p*p*(p*p + s*s*xx*xx)) + (I - xx)/(2*p*p*q*q)
+            + (Math.atan(s*xx/p) + Math.atan(s*(I - xx)/p))/(2*s*p*p*p);
+
+    return f - t;
+}
+
+primitives.convArc = function(x, y, z, center, r, theta, axis, angle, s, t) {
+    var rd = Math.PI/180;
+    var over_i = 0;
+    var over_j = 0;
+    var over_k = 1;
+
+    var cx = center[0];
+    var cy = center[1];
+    var cz = center[2];
+
+    angle += EPS;
+
+    var i = axis[0] + EPS;
+    var j = axis[1] + EPS;
+    var k = axis[2] + EPS;
+
+    var length = Math.sqrt(i*i + j*j + k*k);
+    if (length < EPS) {
+        length = EPS;
+    }
+
+    i /= length;
+    j /= length;
+    k /= length;
+
+    var c = Math.cos(rd*(- angle));
+    var s = Math.sin(rd*(- angle));
+
+    var one_c = 1 - c;
+
+    var ii = i*i;
+    var jj = j*j;
+    var kk = k*k;
+    var ij = i*j;
+    var jk = j*k;
+    var ki = k*i;
+    var is = i*s;
+    var js = j*s;
+    var ks = k*s;
+
+    if (theta > 360) theta = 360;
+    if (theta > 180) {
+        var over_th = (theta - 180)*rd;
+        theta = 180;
+    
+        /* rotate by -angle */
+        var tempx = (c + ii*one_c)*(x - cx) + (-ks + ij*one_c)*(y - cy)
+                    + (js + ki*one_c)*(z - cz);
+        var tempy = (ks + ij*one_c)*(x - cx) + (c + jj*one_c)*(y - cy)
+                    + (-is + jk*one_c)*(z - cz);
+        var tempz = (-js + ki*one_c)*(x - cx) + (is + jk*one_c)*(y - cy)
+                    + (c + kk*one_c)*(z - cz);
+    
+        var over_c = Math.cos(rd*(-180));
+        var over_s = Math.sin(rd*(-180));
+        var over_one_c = 1 - over_c;
+    
+        var over_ii = SQ(over_i);
+        var over_jj = SQ(over_j);
+        var over_kk = SQ(over_k);
+        var over_ij = over_i*over_j;
+        var over_jk = over_j*over_k;
+        var over_ki = over_k*over_i;
+        var over_is = over_i*over_s;
+        var over_js = over_j*over_s;
+        var over_ks = over_k*over_s;
+    
+        var over_x = (over_c + over_ii*over_one_c)*tempx
+                     + (-over_ks + over_ij*over_one_c)*tempy
+                     + (over_js + over_ki*over_one_c)*tempz;
+        var over_y = (over_ks + over_ij*over_one_c)*tempx
+                     + (over_c + over_jj*over_one_c)*tempy
+                     + (-over_is + over_jk*over_one_c)*tempz;
+        var over_z = (-over_js + over_ki*over_one_c)*tempx
+                     + (over_is + over_jk*over_one_c)*tempy
+                     + (over_c + over_kk*over_one_c)*tempz;
+        
+        var a = 2*r*s*s;
+        var d2 = SQ(over_x) + SQ(over_y) + SQ(over_z);
+        var b = 1 + SQ(r)*SQ(s) + SQ(s)*d2;
+        var p2 = -QU(r)*QU(s) + 2*SQ(r)*SQ(s)*(SQ(s)*(d2 - 2*SQ(over_z)) - 1)
+                 - SQ(1 + SQ(s)*d2);
+        var p1 = (p2 < 0) ? Math.sqrt(-p2) : Math.sqrt(p2);
+        var p3 = p1*p2;
+    
+        var f1 = (b*over_y)/(over_x*p2*(a*over_x - b))
+                    + (a*(SQ(over_x) + SQ(over_y))*Math.sin(over_th) - b*over_y) 
+                    /(over_x*p2*(a*(over_x*Math.cos(over_th) + over_y*Math.sin(over_th)) - b));
+        
+        if (p2 < 0) {
+            var f2 = 2.0*b*(Math.atan(-a*over_y/p1)
+                             + Math.atan((a*over_y
+                                 - (a*over_x + b)*Math.tan(over_th/2.0))/p1))/p3;                 
+        } else {
+            var f2 = 2.0*b*(Math.atanh(a*over_y/p1)
+                             + Math.atanh(((a*over_x + b)*Math.tan(over_th/2.0)
+                                - a*over_y)/p1))/p3;
+        }
+        
+        f = f1 + f2;
+    }
+
+    return f - t;
 }
