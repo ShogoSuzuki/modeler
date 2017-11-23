f0affc3512cdace426c9d234d929d87b39342364
Shogo Suzuki <susho0045@iclud.com>
2017-11-23 16:36:11 +0900

    add Primitives

--- src/Primitives.js
+++ src/Primitives.js
@@ -161,7 +161,7 @@ Primitives.block = function(x, y, z, vertex, dx, dy, dz) {
     return (f < zt) ? f : zt;
 }
 
-primitives.blobby = function(x, y, z, center, a, b, t) {
+Primitives.blobby = function(x, y, z, center, a, b, t) {
     var xt = x - center[0];
     var yt = y - center[1];
     var zt = z - center[2];
@@ -171,7 +171,7 @@ primitives.blobby = function(x, y, z, center, a, b, t) {
     return (blobby - t);
 }
 
-primitives.metaball = function(x, y, z, center, b, d, t) {
+Primitives.metaball = function(x, y, z, center, b, d, t) {
     var xt = x - center[0];
     var yt = y - center[1];
     var zt = z - center[2];
@@ -186,7 +186,7 @@ primitives.metaball = function(x, y, z, center, b, d, t) {
     return (mb - t);
 }
 
-primitives.soft = function(x, y, z, center, d, t) {
+Primitives.soft = function(x, y, z, center, d, t) {
     var xt = x - center[0];
     var yt = y - center[1];
     var zt = z - center[2];
@@ -202,14 +202,14 @@ primitives.soft = function(x, y, z, center, d, t) {
     return (soft - t);
 }
 
-primitives.convPoint = function(x, y, z, vect, s, t) {
+Primitives.convPoint = function(x, y, z, vect, s, t) {
     var r2 = SQ(vect[0] - x) + SQ(vect[1] - y) + SQ(vect[2] - z);
     var f = 1/SQ(1 + SQ(s)*r2);
 
     return f - t;
 }
 
-primitives.convLine = function(x, y, z, begin, end, s, t) {
+Primitives.convLine = function(x, y, z, begin, end, s, t) {
     var l = sqrt(SQ(end[0] - begin[0])
              + SQ(end[1] - begin[1]) + SQ(end[2] - begin[2]));
 
@@ -236,7 +236,7 @@ primitives.convLine = function(x, y, z, begin, end, s, t) {
     return f - t;
 }
 
-primitives.convArc = function(x, y, z, center, r, theta, axis, angle, s, t) {
+Primitives.convArc = function(x, y, z, center, r, theta, axis, angle, s, t) {
     var rd = Math.PI/180;
     var over_i = 0;
     var over_j = 0;
@@ -341,7 +341,7 @@ primitives.convArc = function(x, y, z, center, r, theta, axis, angle, s, t) {
     return f - t;
 }
 
-primitives.convTriangle = function(x, y, z, vect, s, T) {
+Primitives.convTriangle = function(x, y, z, vect, s, T) {
     var a1x = vect[0];
     var a1y = vect[1];
     var a1z = vect[2];
@@ -453,7 +453,7 @@ primitives.convTriangle = function(x, y, z, vect, s, T) {
     return f - T;
 }
 
-primitives.convCurve = function(x, y, z, vect, s, t) {
+Primitives.convCurve = function(x, y, z, vect, s, t) {
     var l = Math.sqrt(SQ(vect[0] + 1 - vect[0])
              + SQ(vect[1] + 1 - vect[1]) + SQ(vect[2] + 1 - vect[2]));
 
@@ -478,4 +478,142 @@ primitives.convCurve = function(x, y, z, vect, s, t) {
             + (Math.atan(s*xx/p) + Math.atan(s*(l - xx)/p))/(2*s*p*p*p);
 
     return f - t;
+}
+
+Primitives.convMesh = function(x, y, z, vect, tri, s, t) {
+    var a1x = vect[3*(tri[3] - 1)];
+    var a1y = vect[3*(tri[3] - 1) + 1];
+    var a1z = vect[3*(tri[3] - 1) + 2];
+    var a2x = vect[3*(tri[3 + 1] - 1)];
+    var a2y = vect[3*(tri[3 + 1] - 1) + 1];
+    var a2z = vect[3*(tri[3 + 1] - 1) + 2];
+    var a3x = vect[3*(tri[3 + 2] - 1)];
+    var a3y = vect[3*(tri[3 + 2] - 1) + 1];
+    var a3z = vect[3*(tri[3 + 2] - 1) + 2];
+
+    var len1 = Math.sqrt(SQ(a2x - a1x) + SQ(a2y - a1y) + SQ(a2z - a1z));
+    var len2 = Math.sqrt(SQ(a3x - a2x) + SQ(a3y - a2y) + SQ(a3z - a2z));
+    var len3 = Math.sqrt(SQ(a1x - a3x) + SQ(a1y - a3y) + SQ(a1z - a3z));
+
+    if ((len2 >= len3) && (len2 > len1)) {
+        var tempx = a1x;
+        var tempy = a1y;
+        var tempz = a1z;
+        a1x = a2x;
+        a1y = a2y;
+        a1z = a2z;
+        a2x = a3x;
+        a2y = a3y;
+        a2z = a3z;
+        a3x = tempx;
+        a3y = tempy;
+        a3z = tempz;
+    }  else if ((len3 >= len2) && (len3 > len1)) {
+        var tempx = a1x;
+        var tempy = a1y;
+        var tempz = a1z;
+        a1x = a3x;
+        a1y = a3y;
+        a1z = a3z;
+        a3x = a2x;
+        a3y = a2y;
+        a3z = a2z;
+        a2x = tempx;
+        a2y = tempy;
+        a2z = tempz;
+    }
+    len1 = Math.sqrt(SQ(a2x - a1x) + SQ(a2y - a1y) + SQ(a2z - a1z));
+    len2 = Math.sqrt(SQ(a3x - a2x) + SQ(a3y - a2y) + SQ(a3z - a2z));
+    len3 = Math.sqrt(SQ(a1x - a3x) + SQ(a1y - a3y) + SQ(a1z - a3z));
+
+    var a21x = a2x - a1x;
+    var a21y = a2y - a1y;
+    var a21z = a2z - a1z;
+    var a13x = a1x - a3x;
+    var a13y = a1y - a3y;
+    var a13z = a1z - a3z;
+
+    var t = -(a21x*a13x + a21y*a13y + a21z*a13z)/SQ(len1);
+    var bx = a1x + t*a21x;
+    var by = a1y + t*a21y;
+    var bz = a1z + t*a21z;
+
+    var dx = x - bx;
+    var dy = y - by;
+    var dz = z - bz;
+
+    var ux = a2x - bx;
+    var uy = a2y - by;
+    var uz = a2z - bz;
+    var ul = Math.sqrt(SQ(ux) + SQ(uy) + SQ(uz));
+    ux /= ul;
+    uy /= ul;
+    uz /= ul;
+
+    var vx = a3x - bx;
+    var vy = a3y - by;
+    var vz = a3z - bz;
+    var vl = Math.sqrt(SQ(vx) + SQ(vy) + SQ(vz));
+    vx /= vl;
+    vy /= vl;
+    vz /= vl;
+
+    var d2 = SQ(dx) + SQ(dy) + SQ(dz);
+    var u = dx*ux + dy*uy + dz*uz;
+    var v = dx*vx + dy*vy + dz*vz;
+    var h = Math.sqrt(SQ(a3x - bx) + SQ(a3y - by) + SQ(a3z - bz));
+    var a1 = Math.sqrt(SQ(a1x - bx) + SQ(a1y - by) + SQ(a1z - bz));
+    var a2 = Math.sqrt(SQ(a2x - bx) + SQ(a2y - by) + SQ(a2z - bz));
+
+    var g = v - h;
+    var m = a2*g + u*h;
+    var k = u*h + a1*g;
+    var C2 = 1/SQ(s) + d2 - SQ(u);
+    var C = Math.sqrt(C2);
+    var q = C2 - SQ(v);
+    var w = C2 - 2*v*h + SQ(h);
+    var A2 = SQ(a1)*w + SQ(h)*(q + SQ(u)) - 2*a1*h*u*g;
+    var A = Math.sqrt(A2);
+    var B2 = SQ(a2)*w + SQ(h)*(q + SQ(u)) - 2*a2*h*u*g;
+    var B = Math.sqrt(B2);
+
+    var n1 = a1 + u;
+    var n2 = a2 - u;
+    var n3 = a1*n1 + v*h;
+    var n4 = -a1*u - g*h;
+    var n5 = -a2*n2 - v*h;
+    var n6 = -a2*u + g*h;
+
+    var arc1 = k*(Math.atan(n3/A) + Math.atan(n4/A))/A;
+    var arc2 = m*(Math.atan(n5/B) + Math.atan(n6/B))/B;
+    var arc3 = v*(Math.atan(n1/C) + Math.atan(n2/C))/C;
+    var f = (arc1 + arc2 + arc3)/(2*q*s);
+
+    return f - T;
+}
+
+Primitives.noiseG(x, y, z, amp, frep, phase) {
+    var a2 = amp;
+    var a1 = frep;
+
+    var xt = x;
+    var yt = y;
+    var zt = z;
+
+    var a2x = a2*xt;
+    var a2y = a2*yt;
+    var a2z = a2*zt;
+    var sx = Math.sin(a2x);
+    var sy = Math.sin(a2y);
+    var sz = Math.sin(a2z);
+    var a1d = a1/1.17;
+    var sx2 = a1d*Math.sin(a2x/1.35 + phase*sz);
+    var sy2 = a1d*Math.sin(a2y/1.35 + phase*sx);
+    var sz2 = a1d*Math.sin(a2z/1.35 + phase*sy);
+    var Serx = a1*sx + sx2;
+    var Sery = a1*sy + sy2;
+    var Serz = a1*sz + sz2;
+    var SS = Serx*Sery*Serz;
+
+    return SS;
 }
